c476065d94e3cfda016562e7905473099e60da57
joost <joost.vanoverbeke@gmail.com>
2023-01-13 09:40:00 +0100

    testing

--- src/basic_params_input_ef_tl1.jl
+++ src/basic_params_input_ef_tl1.jl
@@ -4,8 +4,8 @@
 init_values = Init_values(;
     ## ecological input
     # patches
-    grid = (X = 1, Y = 5),
-    torus = (X = :NO, Y = :YES),
+    grid = (X = 1, Y = 1),
+    torus = (X = :NO, Y = :NO),
     env_range = (X = (-0.001, 0.001), Y = (-0.05, 0.05)),
    env_step_CC = 0.0,
    time_CC = 0,
@@ -14,28 +14,28 @@ init_values = Init_values(;
     env_step_local = 0.002,
     dt_env = 1.,
     # dispersal
-    m = 0.01,
+    m = 0.0,
     rho = 2.0,
     m_tl = :EQUAL,
     # resource
-    resource = 220.,
-    in_rate = 220.,
+    resource = 150.,
+    in_rate = 150.,
     out_rate = 0.1,
     # species
     N = 3000,
     spec_dist = :PATCH,
     rep_type = :SEXUAL,
     # trophic levels
-    trophic_levels = 3,
+    trophic_levels = 1,
     bm_offset = 1.,
-    bm_power = 1.,
+    bm_power = 2.,
     # mortality
     d = 0.2,
     d_power = -0.5, #-0.25
     # feeding
     uptake_pars = [0.0001, 0.4, 1.],
     i_power = 0.75, #0.75
-    resource_conversion = 1,
+    resource_conversion = 1.,
     resource_assimilation = 10., 
     assimilation_eff = 0.75, #0.7
     scale_uptake = 2,
@@ -50,10 +50,10 @@ init_values = Init_values(;
 
     ## run input
     runs = 1,
-    pre_post_change = 2500,
+    pre_post_change = 1000,
     print_steps = 1000,
     log_steps = 100,
-    output_file = "results/output_test3.csv"
+    output_file = "results/output_basic_tl1.csv"
 );
 
 # @exportAll()
--- src/basic_params_input_ef_tl2.jl
+++ src/basic_params_input_ef_tl2.jl
@@ -4,27 +4,29 @@
 init_values = Init_values(;
     ## ecological input
     # patches
-    grid = (X = 1, Y = 3),
+    grid = (X = 1, Y = 1),
     torus = (X = :NO, Y = :NO),
     env_range = (X = (-0.001, 0.001), Y = (-0.05, 0.05)),
    env_step_CC = 0.0,
    time_CC = 0,
     # env_step_CC = 0.0004,
     # time_CC = 1000,
-    env_step_local = 0.001,
+    env_step_local = 0.002,
     dt_env = 1.,
     # dispersal
-    m = 0.01,
+    m = 0.0,
     rho = 2.0,
     m_tl = :EQUAL,
     # resource
-    resource = 200.,
-    in_rate = 200.,
+    resource = 100.,
+    in_rate = 100.,
     out_rate = 0.1,
     # species
     N = 3000,
     spec_dist = :PATCH,
     rep_type = :SEXUAL,
+    # species matching
+    match_sd = 1000., 
     # trophic levels
     trophic_levels = 2,
     bm_offset = 1.,
@@ -34,26 +36,26 @@ init_values = Init_values(;
     d_power = -0.5, #-0.25
     # feeding
     uptake_pars = [0.0001, 0.4, 1.],
-    i_power = 0.65, #0.75
+    i_power = 0.75, #0.75
     resource_conversion = 1.,
-    resource_assimilation = 8., 
-    assimilation_eff = 0.8, #0.7
+    resource_assimilation = 10., 
+    assimilation_eff = 0.75, #0.7
     scale_uptake = 2,
-    scale_assim = 0.,
+    scale_assim = 1.,
 
     ## evolutionary input
     omega_e = 0.25,
     trait_loci = 10,
     mu = 1e-4,
     # mu = 0.,
-    sigma_z = 0.1,
+    sigma_z = 0.05,
 
     ## run input
     runs = 1,
-    pre_post_change = 1000,
-    print_steps = 500,
-    log_steps = 100,
-    output_file = "results/output_test5.csv"
+    pre_post_change = 200,
+    print_steps = 20,
+    log_steps = 20,
+    output_file = "results/output_test2.csv"
 );
 
 # @exportAll()
